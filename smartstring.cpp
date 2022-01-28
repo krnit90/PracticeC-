f7fc4e2aa31f82831a133cd1b789be93ad2a7640
krnit90 <47675504+krnit90@users.noreply.github.com>
2022-01-28 07:43:22 +0530

    string class implementation using smart pointer
    unique pointer to store the underlying char buffer in the string class.

---
+++ smartstring.cpp
@@ -0,0 +1,76 @@
+#include<memory>
+#include<iostream>
+#include <cstring>
+class smartstring
+{
+	public:
+		//default constructor
+		smartstring() :pbuf(nullptr), length(0) { }
+
+		//copy constructor
+		smartstring(smartstring& p) : length(p.len()) {
+			pbuf.reset(new char[length + 1]);
+			strncpy_s(pbuf.get(), length + 1, p.pbuf.get(), length);
+		}
+
+		//parameterized constructor
+		//question: why we need explicit  here?
+		explicit smartstring(const char* p) {
+			length = strlen(p);
+			pbuf.reset(new char[length + 1]);
+			strncpy_s(pbuf.get(), length + 1, p, length);
+		}
+		
+		//overloaded assignment operator , called when two objects are assigned
+		smartstring operator = (const smartstring& obj) {
+			if (this != &obj) {
+				length = 0;
+				pbuf.release();
+				length = obj.len();
+				pbuf.reset(new char[length + 1]);
+				strncpy_s(pbuf.get(), length + 1, obj.pbuf.get(), length);
+			}
+			return *this;
+		}
+
+		//overloaded assignment operator , called when smartstring object is assigned a char*
+		void operator = (const char* p)
+		{
+			length = strlen(p);
+			pbuf.reset(new char[length + 1]);
+			strncpy_s(pbuf.get(), length + 1, p, length);
+		}
+
+		//returns the length of the buffer
+		int len() const { return length; }
+
+		//print method
+		void display() { 
+			std::cout << pbuf.get() << "\t" << length << std::endl; 
+		}
+
+	private:
+		std::unique_ptr<char> pbuf;
+		int length;
+};
+
+
+int main()
+{
+	smartstring m("naveen");
+	m.display();
+
+	smartstring n(m);
+	n.display();
+
+	n = m;
+	n.display();
+
+	smartstring s;
+	s = "GUPTA";
+
+	s.display();
+
+
+
+}
